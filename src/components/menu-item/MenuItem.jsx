ab432656b872c7e1db3f0659ef1fd52ae8f1f148
Zeinab Farag <zeinabfarag454@gmail.com>
2021-12-23 05:40:27 -0500

    adding menu item components

---
+++ src/components/menu-item/MenuItem.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import "./menuitem.scss";
+
+const MenuItem = ({ title, imageUrl, size, linkUrl }) => {
+  return (
+    <div
+      style={{ backgroundImage: `url(${imageUrl})` }}
+      className={`menu-item ${size}`}
+    >
+      <div className="content">
+        <h1 className="title">{title}</h1>
+        <span className="subtitle">SHOP NOW</span>
+      </div>
+    </div>
+  );
+};
+
+export default MenuItem;
