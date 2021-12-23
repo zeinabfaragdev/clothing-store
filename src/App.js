2127cf376bf42c97b924415cbc3eb20e84f7ea5c
Zeinab Farag <zeinabfarag454@gmail.com>
2021-12-23 14:12:56 -0500

    adding header

--- src/App.js
+++ src/App.js
@@ -2,14 +2,19 @@ import React from "react";
 import { Route, Routes } from "react-router-dom";
 import Home from "./pages/home/Home";
 import Shop from "./pages/shop/Shop";
+import Header from "./components/header/Header";
 import "./App.css";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/shop" element={<Shop />} />
-    </Routes>
+    <div className="app">
+      <Header />
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/shop" element={<Shop />} />
+      </Routes>
+    </div>
   );
 }
 
